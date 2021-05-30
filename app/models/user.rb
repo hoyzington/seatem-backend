f8fa6035e04c844dfc7ca5c55c76e85f8bbd68b9
Scott Hoisington <hoyzington@gmail.com>
2021-05-29 20:16:03 -0400

    scaffold User; implement User model; namespace controller & route

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # has_many :events, dependent: :destroy
+  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    uniqueness: {case_sensitive: false},
+                    length: {maximum: 105},
+                    format: {with: VALID_EMAIL}
+  before_save {self.email = self.email.downcase}
+  has_secure_password
+end
