ed49be92da606593147bbb755d31ee2e6d049038
Scott Hoisington <hoyzington@gmail.com>
2021-05-31 19:57:58 -0400

    add signup & login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :events, except: [:index, :show]
+      post 'signup', to: 'users#create'
+      post 'login', to: 'users#show'
+      resources :users, except: [:index, :create, :show]
     end
   end
   namespace :api do
     namespace :v1 do
-      resources :users, except: [:index]
+      resources :events, except: [:index, :show]
     end
   end
 end
