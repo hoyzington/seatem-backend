b1404389669914957d6ab8dbc08b0c5f8896177a
Scott Hoisington <hoyzington@gmail.com>
2021-05-31 00:17:43 -0400

    add auth to User show; fix user variable in create; remove show from before_action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,22 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: [:update, :destroy]
 
   # GET /api/v1/users/1
   def show
-    user_json = UserSerializer.new(@user).serialized_json
-    render json: user_json, status: :accepted
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      user_json = UserSerializer.new(user).serialized_json
+      render json: user_json, status: :accepted
+    else
+      render json: { message: "Incorrect username and/or password" }
+    end
   end
 
   # POST /api/v1/users
   def create
     user = User.new(user_params)
     if user.save
-      user_json = UserSerializer.new(@user).serialized_json
+      user_json = UserSerializer.new(user).serialized_json
       render json: user_json, status: :created, location: user
     else
       render json: user.errors, status: :unprocessable_entity
