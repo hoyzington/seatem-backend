dc43184505585d22dfb714fe48446a2172833345
Scott Hoisington <hoyzington@gmail.com>
2021-05-31 00:10:15 -0400

    use serializer for create & update actions

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,9 +10,9 @@ class Api::V1::UsersController < ApplicationController
   # POST /api/v1/users
   def create
     user = User.new(user_params)
-
     if user.save
-      render json: user, status: :created, location: user
+      user_json = UserSerializer.new(@user).serialized_json
+      render json: user_json, status: :created, location: user
     else
       render json: user.errors, status: :unprocessable_entity
     end
@@ -21,7 +21,8 @@ class Api::V1::UsersController < ApplicationController
   # PATCH/PUT /api/v1/users/1
   def update
     if user.update(user_params)
-      render json: user, status: :updated
+      user_json = UserSerializer.new(@user).serialized_json
+      render json: user_json, status: :updated
     else
       render json: user.errors, status: :unprocessable_entity
     end
