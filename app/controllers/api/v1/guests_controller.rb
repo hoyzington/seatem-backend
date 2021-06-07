7e7c4e65451b274b8d4fd5256f74f9c245ba45a6
Scott Hoisington <hoyzington@gmail.com>
2021-06-07 14:25:29 -0400

    customize action implementation

--- app/controllers/api/v1/guests_controller.rb
+++ app/controllers/api/v1/guests_controller.rb
@@ -1,43 +1,33 @@
 class Api::V1::GuestsController < ApplicationController
   before_action :set_guest, only: [:show, :update, :destroy]
 
-  # GET /api/v1/guests
-  def index
-    @guests = Guest.all
-    render json: @guests
-  end
-
-  # GET /api/v1/guests/1
-  def show
-    render json: @guest
-  end
-
   # POST /api/v1/guests
   def create
-    @guest = Guest.new(guest_params)
-
-    if @guest.save
-      render json: @guest, status: :created, location: @guest
+    guest = Guest.new(guest_params)
+    if guest.save
+      render json: guest, except: [:created_at, :updated_at] status: :created, location: guest
     else
-      render json: @guest.errors, status: :unprocessable_entity
+      render json: { errors: guest.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /api/v1/guests/1
   def update
     if @guest.update(guest_params)
-      render json: @guest
+      render json: { notice: `#{@guest.name} has been updated` }, status: :created
     else
-      render json: @guest.errors, status: :unprocessable_entity
+      render json: { errors: @guest.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   # DELETE /api/v1/guests/1
   def destroy
     @guest.destroy
+    render json: { notice: `#{event.name} has been deleted` }, status: :destroyed
   end
 
   private
+
     def set_guest
       @guest = Guest.find(params[:id])
     end
