839a0cf34b7eaf30b11533083b5bb764715597bc
Scott Hoisington <hoyzington@gmail.com>
2021-06-15 19:40:47 -0400

    use serializer methods

--- app/controllers/api/v1/guests_controller.rb
+++ app/controllers/api/v1/guests_controller.rb
@@ -1,26 +1,29 @@
 class Api::V1::GuestsController < ApplicationController
-  before_action :set_guest, only: [:show, :update, :destroy]
+  before_action :set_guest, only: [:update, :destroy]
 
-  # POST /api/v1/users/1/events/1/guests
+  # POST /api/v1/events/1/guests
   def create
-    guest = Guest.new(guest_params)
+    event = Event.find(params[:event_id])
+    guest = event.guests.build(guest_params)
     if guest.save
-      render json: guest, except: [:created_at, :updated_at] status: :created
+      guest_json = GuestSerializer.new(guest).to_serialized_json
+      render json: guest_json, status: :partial_content
     else
       render json: { errors: { type: 'newGuest', content: guest.errors.full_messages } }, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /api/v1/users/1/events/1/guests/1
+  # PATCH/PUT /api/v1/guests/1
   def update
     if @guest.update(guest_params)
-      render json: { notice: `#{@guest.name} has been updated` }, status: :partial_content
+      guest_json = GuestSerializer.new(@guest).to_serialized_json
+      render json: guest_json, status: :partial_content
     else
       render json: { errors: { type: 'guestUpdate', content: @guest.errors.full_messages } }, status: :unprocessable_entity
     end
   end
 
-  # DELETE /api/v1/users/1/events/1/guests/1
+  # DELETE /api/v1/guests/1
   def destroy
     @guest.destroy
     render json: { notice: `#{@guest.name} has been deleted` }, status: :partial_content
