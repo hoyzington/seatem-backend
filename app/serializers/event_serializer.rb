af5dde7ccd3ad2a69a69eabbe7d73f41bb38d80b
Scott Hoisington <hoyzington@gmail.com>
2021-06-15 19:38:56 -0400

    add to_serialized_json_with_guests

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -4,10 +4,14 @@ class EventSerializer
   end
 
   def to_serialized_json
+    @event.to_json(except: [:created_at, :user_id])
+  end
+
+  def to_serialized_json_with_guests
     @event.to_json(
       include: {
         guests: {
-          except: [:created_at, :event_id]
+          except: [:created_at, :updated_at, :event_id]
         }
       },
       except: [:created_at, :user_id])
