fc89a7dba0d760149913a8447c47c68127f70fcb
Scott Hoisington <hoyzington@gmail.com>
2021-10-17 22:55:41 -0400

    add helper method; lint with rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
   include ::ActionController::Cookies
 
@@ -7,5 +9,14 @@ class ApplicationController < ActionController::API
 
   def logged_in?
     !!current_user
+  end
+
+  def render_errors(error_type, error_content)
+    render json: {
+      errors: {
+        type: error_type,
+        content: error_content
+      }
+    }, status: :unprocessable_entity
   end
 end
